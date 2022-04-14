537116af22afb8aa0b8f49bb5614d9d552e02704
Yash Siddham <yashsiddham@gmail.com>
2022-04-15 02:52:18 +0530

    Created Specialty and added it to Vet. Closes #21

--- pet-clinic-data/src/main/java/com/siddhamyash/petclinicspring/model/Vet.java
+++ pet-clinic-data/src/main/java/com/siddhamyash/petclinicspring/model/Vet.java
@@ -1,4 +1,16 @@
 package com.siddhamyash.petclinicspring.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }

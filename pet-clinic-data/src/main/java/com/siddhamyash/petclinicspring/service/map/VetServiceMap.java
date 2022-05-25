8c63441070241bfd180df26893b759ba3ad21636
Yash Siddham <yashsiddham@gmail.com>
2022-05-25 14:48:28 +0530

    Rewritten save method of Vet to save specialities. Closes #28

--- pet-clinic-data/src/main/java/com/siddhamyash/petclinicspring/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/siddhamyash/petclinicspring/service/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.siddhamyash.petclinicspring.service.map;
 
+import com.siddhamyash.petclinicspring.model.Specialty;
 import com.siddhamyash.petclinicspring.model.Vet;
+import com.siddhamyash.petclinicspring.service.SpecialityService;
 import com.siddhamyash.petclinicspring.service.VetService;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Component
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -27,8 +36,26 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if(object != null){
+            if(object.getSpecialties() != null){
+                object.getSpecialties().forEach(speciality -> {
+                    if(speciality.getDescription() != null){
+                        if(speciality.getId() == null){
+                            Specialty savedSpeciality = specialityService.save(speciality);
+                            speciality.setId(savedSpeciality.getId());
+                        }
+
+                    }
+                });
+            }else{
+                throw new RuntimeException("Speciality is required");
+            }
+            return super.save(object);
+        }else{
+            throw new RuntimeException("A valid object is required");
+        }
 //        object.setId(object.getId()+1000);
-        return super.save(object);
     }
 
     @Override
